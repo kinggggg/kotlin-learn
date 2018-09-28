3befb2b2db20333844407f1d57eccfa2d413ba9b
kinggggg <liweibooooo@126.com>
2018-09-28 13:56:33 +0800

    整形学习

--- src/helloWorld.kt
+++ src/helloWorld.kt
@@ -30,4 +30,23 @@ fun main(args: Array<String>) {
     var str2 = "我时字符串2" ;
     println("2")
     println(str2);
+
+    //定义整形变量
+    var i : Int = 10;
+    //赋值为null的话直接编译不通过
+//    var i0 : Int = null;
+    //赋值为null的话，需要加上？不加？会映射成java中的基本类型；加上？会映射成为java中对应类型的包装类型
+    var i1 : Int? = null;
+    var i2 : Int = 200;
+    var i3 : Int = 200;
+    println(i2 === i3); //true 相当于两个java中的int进行比较
+    println(i2 == i3); //true 相当于两个java中的int进行比较
+    var i4 : Int? = 300;
+    var i5 : Int? = 300;
+    println(i4 === i5); //false 相当于两个java中的Integer进行 对象地址的比较
+    println(i4 == i5); //true 相当于两个java中的Integer对应的int值进行比较
+    var i6 : Int? = 100;
+    var i7 : Int? = 100;
+    println(i6 === i7); //和java中的 -127 ~ 128的效果一样 有常量池在做影响
+    println(i6 == i7);
 }
